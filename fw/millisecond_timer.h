6a7cabc15da8e1559daf83ffd1aea70134401b12
Josh Pieper <jjp@pobox.com>
2019-12-01 07:46:35 -0500

    Don't rely on an uninitialized timer init structure

--- fw/millisecond_timer.h
+++ fw/millisecond_timer.h
@@ -26,7 +26,7 @@ class MillisecondTimer {
     handle_.Instance = TIM5;
     handle_.Init.Period = 0xFFFFFFFF;
     handle_.Init.Prescaler =
-        (uint32_t)(SystemCoreClock / 2U / 1000000U) - 1;  // 1 us tick
+        (uint32_t)(SystemCoreClock / 1000000U) - 1;  // 1 us tick
     handle_.Init.ClockDivision = 0;
     handle_.Init.CounterMode = TIM_COUNTERMODE_UP;
     handle_.Init.RepetitionCounter = 0;
@@ -61,7 +61,7 @@ class MillisecondTimer {
   }
 
  private:
-  TIM_HandleTypeDef handle_;
+  TIM_HandleTypeDef handle_ = {};
 };
 
 }
