593c3231240dcd4cf6fe44601f13736bea40c7f4
Josh Pieper <jjp@pobox.com>
2025-09-17 12:02:25 -0400

    Update the FIFO so that it can be used in its entirety.

--- fw/fifo.h
+++ fw/fifo.h
@@ -21,7 +21,7 @@ template<typename T, size_t size>
 class Fifo {
  public:
   bool full() const {
-    return ((put_index_ + 1) % size) == get_index_;
+    return static_cast<uint32_t>((put_index_ - get_index_) == size);
   }
 
   bool empty() const {
@@ -29,29 +29,29 @@ class Fifo {
   }
 
   void push(T&& value) {
-    data_[put_index_] = std::move(value);
-    put_index_ = (put_index_ + 1) % size;
+    data_[put_index_ % size] = std::move(value);
+    put_index_ = put_index_ + 1;
   }
 
   const T& top() const {
-    return data_[get_index_];
+    return data_[get_index_ % size];
   }
 
   void push(const T& value) {
     if (full()) { return; }
 
-    data_[put_index_] = value;
-    put_index_ = (put_index_ + 1) % size;
+    data_[put_index_ % size] = value;
+    put_index_ = put_index_ + 1;
   }
 
   T pop() {
     const auto old_get_index = get_index_;
-    get_index_ = (get_index_ + 1) % size;
-    return data_[old_get_index];
+    get_index_ = get_index_ + 1;
+    return data_[old_get_index % size];
   }
 
   T data_[size] = {};
-  size_t put_index_ = 0;
-  size_t get_index_ = 0;
+  uint32_t put_index_ = 0;
+  uint32_t get_index_ = 0;
 };
 }
